2e9f690811a6399bd0c3b3ce84cbf78154704b87
Wilfredo.Laboy <onepromise@gmail.com>
2014-09-18 21:33:18 -0400

    broke my code
    need to start from scratch =(

---
+++ GA_stop_light/js/myscript.js
@@ -0,0 +1,9 @@
+function buttonclick(on){
+var redLight = document.getElementById('redlight')
+var YellowLight = document.getElementById('yellowlight')
+var GreenLight = document.getElementById('greenlight')
+
+redLight.style.backgroundColor ="darkred";
+yellowLight.style.backgroundColor ="#CC9900";
+greenLight.style.backgroundColor ="#003300";
+}
