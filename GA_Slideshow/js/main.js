c97c0e687bea3afbddd85047b3915af641c3c584
Wilfredo.Laboy <onepromise@gmail.com>
2014-10-02 18:31:29 -0400

    Fixed many of the issues with the layout for slideshow
    Still having issues with the JS. Still messing with it and trying to get
    it to work. I am missing a few steps. blah lol

--- GA_Slideshow/js/main.js
+++ GA_Slideshow/js/main.js
@@ -1,10 +1,10 @@
-var images =[""]
+var images =["1.jpg, 2.jpg, 3.jpg, 4.jpg, 5.jpg, 6.jpg, 7.jpg, 8.jpg"]
 var currentImage = 0
 
-function changePicture(whichNum){
+function changePicture(picNum){
 	var img = document.getElementById('mainImage');
-	img.setAttribute('src', images[whichNum]);
-	currentImage =	whichNum;
+	img.setAttribute('src', images[picNum]);
+	currentImage =	picNum;
 }
 function nextImage(){
 	currentImage++;
@@ -13,5 +13,5 @@ function nextImage(){
 }
 function prevImage(){
 	currentImage = currentImage - 1;
-	img.setAttribute('src', images[whichNum]);
+	img.setAttribute('src', images[picNum]);
 }
