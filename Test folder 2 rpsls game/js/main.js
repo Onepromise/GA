fcf65de67224a428fce30f28d92d8426917c8bbb
Onepromise <onepromise@gmail.com>
2014-10-03 14:17:15 -0400

    Created a test folder for game hw

---
+++ Test folder 2 rpsls game/js/main.js
@@ -0,0 +1,69 @@
+$(function() {
+    $("#radio").buttonset();
+});
+
+function fight() {
+    var randomChoice = Math.random();
+    var threeChoice = Math.floor(randomChoice * 3);
+    var weapon = document.getElementsByName("weapon");
+
+    for (i = 0; i < weapon.length; i++) {
+        currentWeapon = weapon[i];
+
+        if (currentWeapon.checked) {
+            var selectedWeapon = currentWeapon.value;
+        } // end if
+    } // end for
+    var cpuChoice = new Array("Paper", "Rock", "Scissors")
+    var reply = "xxx"
+    if (threeChoice === 0) {
+        if (selectedWeapon === "Paper") {
+            reply = "It's a draw";
+        }
+        else if (selectedWeapon === "Rock") {
+            reply = "Paper covers Rock, You Loose!";
+        }
+        else if (selectedWeapon === "Scissors") {
+            reply = "Paper is shredded by Scissors, You Win!";
+        }
+        else {
+            reply = "what1";
+        }
+    } else if (threeChoice === 1) {
+        if (selectedWeapon === "Paper") {
+            reply = "Paper covers Rock, You Win!";
+        }
+        else if (selectedWeapon === "Rock") {
+            reply = "Tie, try it again!";
+        }
+        else if (selectedWeapon === "Scissors") {
+            reply = "Rock smashes Scissors, You Loose!";
+        }
+        else {
+            reply = "what2";
+        }
+    } else if (threeChoice === 2) {
+        if (selectedWeapon === "Paper") {
+            reply = "Scissors cut paper, your screwed";
+        }
+        else if (selectedWeapon === "Rock") {
+            reply = "I can't believe TURTLEWOLF wrote a video game! oh and you won...";
+        }
+        else if (selectedWeapon === "Scissors") {
+            reply = "Oh no, not again!";
+        }
+        else {
+            reply = "what3";
+        }
+    } else {
+        reply = "hay now?";
+    }
+    var output = document.getElementById("output");
+    var response = "<h2>You have chosen ";
+    response += selectedWeapon + "<h2>The CPU has chosen ";
+    "<\/h2> \n";
+    response += cpuChoice[threeChoice] + "<\/h2> \n";
+    response += reply + "<\/h2> \n";
+    output.innerHTML = response;
+} // end function
+//]]>
