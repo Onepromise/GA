5838c31e395d3ad48fa10ee00c5ccc9cc217d270
Onepromise <onepromise@gmail.com>
2014-10-21 16:23:32 -0400

    added the onclick feature!

--- Final Project Test/js/main.js
+++ Final Project Test/js/main.js
@@ -2,8 +2,14 @@
 //var userName = prompt("What is your first name?");
 //var userage = prompt("How old are you?");
 //if (userage < 18){
-	 alert(userName + ", you might be too young to enter my domain, I am not held responsible if you continue on.")
-}
-else{
-	 alert("Prepare yourself, " + userName + "!")
+//	 alert(userName + ", you might be too young to enter my domain, I am not held responsible if you continue on.")
+//}
+//else{
+//	 alert("Prepare yourself, " + userName + "!")
+//}
+function choice1() {
+    var x = document.createElement("P");
+    var t = document.createTextNode("This is a paragraph.");
+    x.appendChild(t);
+    document.getElementById("bodyText").appendChild(x);
 }
