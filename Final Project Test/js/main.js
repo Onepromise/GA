50d0f964993c58770d7a890cd4c4032eb99fb862
Onepromise <onepromise@gmail.com>
2014-10-21 15:43:57 -0400

    Final Project
    added the intro prompts broke some other stuff, but fixed it. Added a
    cool shake feature. Still working on adding text via JS to progress the
    story.

--- Final Project Test/js/main.js
+++ Final Project Test/js/main.js
@@ -1,3 +1,9 @@
-function myFunction() {
-    document.getElementById("choice1").style.color = "red";
+//alert("Welcome to the world I have created for you. In this text world, your decision chooses your outcome. Are you ready?");
+//var userName = prompt("What is your first name?");
+//var userage = prompt("How old are you?");
+//if (userage < 18){
+	 alert(userName + ", you might be too young to enter my domain, I am not held responsible if you continue on.")
+}
+else{
+	 alert("Prepare yourself, " + userName + "!")
 }
