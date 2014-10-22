e61114d17aaf41e0d3b461eba984c7b53c1aeddb
Onepromise <onepromise@gmail.com>
2014-10-22 09:09:30 -0400

    changed rps game.
    Trying a different method to get it to work properly and make it more
    simplistic

--- GA_rps_game/js/main.js
+++ GA_rps_game/js/main.js
@@ -1,17 +1,44 @@
-function duel()
-	var randomChoice = Math.random();
-	var fiveChoice = Math.floor(randomChoice * 5);
-	var choice = document.getElementByClass("button");
+var choices  =  {rock : {name: "Rock", defeats: ["scissors","lizard"]},
+                 paper: {name: "Paper", defeats: ["rock", "spock"]},
+                 scissors: {name: "Scissors", defeats: ["paper", "lizard"]},
+                 lizard: {name: "Lizard", defeats:["paper","spock"]},
+                 spock: {name: "Spock", defeats:["scissors","rock"]}
+                };
 
-	for (i = 0; i <choice.length; i++) {
-		currentChoice = choice[i];
-	}
-		if(currentChoice = function abilityChoice()){
-			var selectedChoice = currentChoice.id;
-		}
-	}	
-	var npcChoice = new Array("Paper", "Rock", "Scissors", "Lizard", "Spock")
-	var reply = ""
-	if (fiveChoice == 0){}
-		if (selectedChoice == "Paper")
-	}
+
+var computerChoice = Math.random();
+if (computerChoice < 0.2) {
+    computerChoice = "rock";
+} else if (computerChoice <= 0.4) {
+    computerChoice = "paper";
+} else if (computerChoice <= 0.6) {
+    computerChoice = "scissors";
+} else if (computerChoice <= 0.8) {
+    computerChoice = "lizard";
+} else {
+    computerChoice = "spock";
+}
+
+
+var userChoice = prompt("Do you choose rock, paper, scissors, lizard, or spock?").toLowerCase();
+
+alert("The computer chose " + computerChoice);
+
+
+
+if(computerChoice == userChoice){
+    alert("It's a tie");
+}else if(choices[userChoice] === undefined){
+    alert("Invalid Choice");
+}else{
+    userChoice = choices[userChoice];
+    
+    var victory = userChoice.defeats.indexOf(computerChoice) > -1;
+    
+    if(victory) {
+        alert("Vitory! " + userChoice.name + " wins!")
+    }else{
+        alert("Defeat, " + computerChoice + " wins!");
+    }   
+}
+    
