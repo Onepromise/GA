ae1aa841c9931743b5ae6fab68702c8080b25e58
Wilfredo.Laboy <onepromise@gmail.com>
2014-10-21 21:32:22 -0400

    better with the js

--- Final Project Test/js/main.js
+++ Final Project Test/js/main.js
@@ -1,3 +1,6 @@
+var choices =[cforest, chighway, chouse1, chouse2, chouse3, crv, cenp, cwwt, cwal1, cwal2, cwal3]
+var story =[intro, forest, highway, house1, house2, house3, rv, enp, wwt, wal1, wal2, wal3]
+
 //alert("Welcome to the world I have created for you. In this text world, your decision chooses your outcome. Are you ready?");
 //var userName = prompt("What is your first name?");
 //var userage = prompt("How old are you?");
@@ -9,7 +12,13 @@
 //}
 function choice1() {
     var x = document.createElement("P");
-    var t = document.createTextNode("This is a paragraph.");
+    var t = document.createTextNode(forest);
+    x.appendChild(t);
+    document.getElementById("bodyText").appendChild(x);
+}
+function choice2() {
+    var x = document.createElement("P");
+    var t = document.createTextNode(highway);
     x.appendChild(t);
     document.getElementById("bodyText").appendChild(x);
 }
