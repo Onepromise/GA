c1ef1ba7c801bf72c18b0e5f8021b91e0c7fe947
Wilfredo.Laboy <onepromise@gmail.com>
2014-10-27 08:26:17 -0400

    final changes for final project
    Just need to finish the story and connect it!

--- Final Project Test/js/choices.js
+++ Final Project Test/js/choices.js
@@ -1,34 +1,11 @@
 var intro ="I am the intro. I am the start. you ran for you life now you have to choose between, running through the forst or running through the highway. Choose your path!"
-
-var cforest ="Go through the forest"
-var chighway ="Go to the highway"
-
 var forest = "You decided to go through the forest and try and avoid heavy populated areas. You head through the forest and make your way to the where you remember passing by some houses. As you travel through the forest you hear a loud explosion,and you see a huge smoke cloud through the clearing. It is in the same direction as the houses and you can start to see the houses as you almost exit the forest. You see 4 houses and 1 is on fire, probably the explosion you heard. Which house do you go to house 1 is closest to the fire, house 2 is the second closest to the fire and the third is the furest away from the house on fire but closest to the main road"
-
-var chouse1 ="" 
-var chouse2 ="" 
-var chouse3 =""
-var crv =""
-var cenp =""
-var cwwt =""
-var cwal1 =""
-var cwal2 =""
-var cwal3 =""
-
-var highway =""
-var house1 =""
-var house2 =""
-var house3 =""
-var rv =""
+var highway ="You decide to go to the high way"
+var house1 ="You pick house 1"
+var house2 ="You pick house 2"
+var rv ="Look an RV"
 var enp =""
 var wwt =""
 var wal1 =""
 var wal2 =""
 var wal3 =""
-
-//var x = document.createElement("P");
-  //  var t = document.createTextNode(forest);
-    //var c1 = document.getElementById("c1");
-    //x.appendChild(t);
-    //document.getElementById("bodyText").appendChild(x);
-    //c1.innerHTML = "House 1";
--- Final Project Test/js/main.js
+++ Final Project Test/js/main.js
@@ -1,5 +1,4 @@
-var choices =[cforest, chighway, chouse1, chouse2, chouse3, crv, cenp, cwwt, cwal1, cwal2, cwal3]
-var story =[intro, forest, highway, house1, house2, house3, rv, enp, wwt, wal1, wal2, wal3]
+var story =[intro, forest, highway, house1, house2, rv, enp, wwt, wal1, wal2, wal3]
 
 alert("Welcome to the world I have created for you. In this text world, your decision chooses your outcome. Are you ready?");
 var userName = prompt("What is your first name?");
@@ -10,22 +9,140 @@ if (userage < 18){
 else{
 	 alert("Prepare yourself, " + userName + "!") ;
 }
-function choice1() {
+function choice1(){
+    document.getElementById("s2").style.display = 'block';
+    document.getElementById("s1").style.display = 'none';
     var x = document.createElement("P");
-    var t = document.createTextNode(forest);
-    var c1 = document.getElementById("c1");
-    var c2 = document.getElementById("c2")
-    x.appendChild(t);
-    document.getElementById("bodyText").appendChild(x);
-    c1.innerHTML = "House 1";
-    c2.innerHTML = "House 2";
-
-
-
-} 
-function choice2() {
-    var x = document.createElement("P");
-    var t = document.createTextNode(highway);
-    x.appendChild(t);
+    var s = document.createTextNode(intro);
+    x.appendChild(s);
     document.getElementById("bodyText").appendChild(x);
 }
+function choice2(){
+    document.getElementById("s2").style.display = 'block';
+    document.getElementById("s1").style.display = 'none';
+    var x = document.createElement("P");
+    var s = document.createTextNode(intro);
+    x.appendChild(s);
+    document.getElementById("bodyText").appendChild(x);
+}
+function choice3(){
+    document.getElementById("s3").style.display = 'block';
+    document.getElementById("s2").style.display = 'none';
+    var x = document.createElement("P");
+    var s = document.createTextNode(forest);
+    x.appendChild(s);
+    document.getElementById("bodyText").appendChild(x);
+    document.body.style.backgroundImage="url('img/forest.jpg')";
+}
+function choice4(){
+    document.getElementById("s4").style.display = 'block';
+    document.getElementById("s3").style.display = 'none';
+    document.body.style.backgroundImage="url('img/highway.jpg')";
+}
+function choice5(){
+    document.getElementById("s5").style.display = 'block';
+    document.getElementById("s3").style.display = 'none';
+}
+function choice6(){
+    document.getElementById("s6").style.display = 'block';
+    document.getElementById("s3").style.display = 'none';
+}
+function choice7(){
+    document.getElementById("s11").style.display = 'block';
+    document.getElementById("s4").style.display = 'none';
+}
+function choice8(){
+    document.getElementById("s12").style.display = 'block';
+    document.getElementById("s4").style.display = 'none';
+}
+function choice9(){
+    document.getElementById("s7").style.display = 'block';
+    document.getElementById("s5").style.display = 'none';
+}
+function choice10(){
+    document.getElementById("s8").style.display = 'block';
+    document.getElementById("s5").style.display = 'none';
+}
+function choice11(){
+    document.getElementById("s9").style.display = 'block';
+    document.getElementById("s6").style.display = 'none';
+}
+function choice12(){
+    document.getElementById("s10").style.display = 'block';
+    document.getElementById("s6").style.display = 'none';
+}
+function choice13(){
+    document.getElementById("s11").style.display = 'block';
+    document.getElementById("s7").style.display = 'none';
+}
+function choice14(){
+    document.getElementById("s8").style.display = 'block';
+    document.getElementById("s7").style.display = 'none';
+}
+function choice15(){
+    document.getElementById("s8").style.display = 'block';
+    document.getElementById("s5").style.display = 'none';
+}
+function choice16(){
+    document.getElementById("s8").style.display = 'block';
+    document.getElementById("s5").style.display = 'none';
+}
+function choice17(){
+    document.getElementById("s2").style.display = 'block';
+    document.getElementById("s1").style.display = 'none';
+}
+function choice18(){
+    document.getElementById("s2").style.display = 'block';
+    document.getElementById("s1").style.display = 'none';
+}
+function choice19(){
+    document.getElementById("s3").style.display = 'block';
+    document.getElementById("s2").style.display = 'none';
+}
+function choice20(){
+    document.getElementById("s4").style.display = 'block';
+    document.getElementById("s3").style.display = 'none';
+}
+function choice21(){
+    document.getElementById("s5").style.display = 'block';
+    document.getElementById("s3").style.display = 'none';
+}
+function choice22(){
+    document.getElementById("s6").style.display = 'block';
+    document.getElementById("s3").style.display = 'none';
+}
+function choice23(){
+    document.getElementById("s11").style.display = 'block';
+    document.getElementById("s4").style.display = 'none';
+}
+function choice24(){
+    document.getElementById("s12").style.display = 'block';
+    document.getElementById("s4").style.display = 'none';
+}
+function choice25(){
+    document.getElementById("s7").style.display = 'block';
+    document.getElementById("s5").style.display = 'none';
+}
+function choice26(){
+    document.getElementById("s8").style.display = 'block';
+    document.getElementById("s5").style.display = 'none';
+}
+function choice27(){
+    document.getElementById("s8").style.display = 'block';
+    document.getElementById("s5").style.display = 'none';
+}
+function choice28(){
+    document.getElementById("s8").style.display = 'block';
+    document.getElementById("s5").style.display = 'none';
+}
+function choice29(){
+    document.getElementById("s8").style.display = 'block';
+    document.getElementById("s5").style.display = 'none';
+}
+function choice30(){
+    document.getElementById("s8").style.display = 'block';
+    document.getElementById("s5").style.display = 'none';
+}
+//function choice31(){
+    //document.getElementById("").style.display = 'block';
+    //document.getElementById("").style.display = 'none';
