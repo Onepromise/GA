9535aff01edb73f966fd43cdea3ea05ea56dca42
Onepromise <onepromise@gmail.com>
2014-10-30 16:59:53 -0400

    10/30

--- Final Project Test/js/main.js
+++ Final Project Test/js/main.js
@@ -1,12 +1,12 @@
-alert("Welcome to the world I have created for you. In this text world, your decision chooses your outcome. Are you ready?");
-var userName = prompt("What is your first name?");
-var userage = prompt("How old are you?");
-if (userage < 18){
-	 alert(userName + ", you might be too young to enter my domain, I am not held responsible if you continue on.")
-}
-else{
-	 alert("Prepare yourself, " + userName + "!") ;
-}
+//alert("Welcome to the world I have created for you. In this text world, your decision chooses your outcome. Are you ready?");
+//var userName = prompt("What is your first name?");
+//var userage = prompt("How old are you?");
+//if (userage < 18){
+//	 alert(userName + ", you might be too young to enter my domain, I am not held responsible if you continue on.")
+//}
+//else{
+//	 alert("Prepare yourself, " + userName + "!") ;
+//}
 function choice1(){
     document.getElementById("s2").style.display = 'block';
     document.getElementById("s1").style.display = 'none';
